7898c84ba6983b2102bbabb938c329d6906f214e
EC2 Default User <ec2-user@ip-172-31-25-139.ec2.internal>
2019-03-26 14:21:22 +0000

    eft call works. need add report.

--- def.php
+++ def.php
@@ -36,14 +36,70 @@ define("eft_ReqURL", ReqURLBase.ReqVer.eft_ReqFun, TRUE);
 // Reqeust Auth
 define ( 'AUTH','Passcode '. base64_encode (MID.':'.APIKEY));
 
-//eft_params
-define ( 'eft_EB_CRITERIA', BOUNDARY_STRING.'
-                    Content-Disposition: form-data; name="criteria"
-                    Content-Type: application/json' );
-define ( 'eft_EB_DATA'  ,  BOUNDARY_STRING.'
-                    Content-Disposition: form-data; name="data", filename="eft_transactions.csv"
-                    Content-Type: text/plain' );
+//eft record class
+class EFT_Class
+{
+        private
+                $type= 'E',                     //Transaction type - The type of transaction.E - EFT
+                $creditdebit='D'                //Transaction type ,C – Credit recipient bank accounts,D – Debit an outside bank account and depositing funds into your own
+                ;
+        public
+                $bank='',                       //Financial institution number - The 3 digit financial institution number
+                $transit='',                    //Bank transit number - The 5 digit bank transit number
+                $acc='',                        //Account number - The 5-12 digit account number
+                $amt='',                        //Amount - Transaction amount in pennies
+                $ref='',                        //Reference number - An optional reference number of up to 19 digits. If you don't want a reference number, enter "0" (zero).
+                $name='',                       //Recipient name - Full name of the bank account holder
+                $code='',                       //Customer code - The 32-character customer code located in the Payment Profile. Do not populate bank account fields in the file when processing against a Payment Profile.
+                $desc=''                        //By default the Bambora merchant company name will show on your customer's bank statement. You can override this default by populating the Dynamic Descriptor field.
+                ;
+
+        function debit(){
+            $creditdebit = 'D';
+        }
+        
+        function credit(){
+            $creditdebit = 'C';
+        }
 
 
+};
+
+
+//eft_params in array 
+$my_eft_records_array = [
+        [
+                'type'=> 'E',               //Transaction type - The type of transaction.E - EFT
+                'credit-debit'=>'D',        //Transaction type ,C – Credit recipient bank accounts,D – Debit an outside bank account and depositing funds into your own
+                'bank'=>'',                 //Financial institution number - The 3 digit financial institution number
+                'transit'=>'',              //Bank transit number - The 5 digit bank transit number
+                'acc'=>'',                  //Account number - The 5-12 digit account number
+                'amt'=>'',                  //Amount - Transaction amount in pennies
+                'ref'=>'',                  //Reference number - An optional reference number of up to 19 digits. If you don't want a reference number, enter "0" (zero).
+                'name'=>'',                 //Recipient name - Full name of the bank account holder
+                'code'=>'',                 //Customer code - The 32-character customer code located in the Payment Profile. Do not populate bank account fields in the file when processing against a Payment Profile.
+                'desc'=>'',                 //By default the Bambora merchant company name will show on your customer's bank statement. You can override this default by populating the Dynamic Descriptor field.     
+           ],                
+        ];
+
+
+//eft_params in string 
+//Note: in the following sequence
+//type,credit-debit,bank,transit,acc,amt,ref,name,code,desc,
+//example ( no title line):
+//
+//E,C,001,99001,09400313371,10000,1000070001,ACME Corp,123,desc1
+//E,C,002,99002,09400313372,20000,1000070002,John Doe,234,desc2
+//E,C,003,99003,09400313373,30000,1000070003,Jane Doe,345,desc3
+$my_eft_records_str = '';
+
+
+//eft process control !!CREDITCARD only!!
+
+//initialization
+$process_now =1;		 //**Credit card transactions** only, process now (1) or other date(0), overrides process_date parameter and processes transactions on receipt. .
+$process_date = '';      //when process_now == 0,The date the transactions starts processing. Format YYYYMMDD.
+$sub_merchant_id = '';   //the merchant account on which to process the transactions. Required only if request is authorized by a partner account.
+$addendum = '';          //note associated with the batch file. Max length: 80 characters.
 
 ?>
--- my_def.php
+++ my_def.php
@@ -25,36 +25,11 @@ SOFTWARE.
 // API Customized Params
 // need change to your own credentials
 define("MID","300206397",TRUE);                                 //need change
-
-define("APIKEY","13E07AeeBF32411DBEB15E413eCDbAbA",TRUE);       //need change to your **Batch File Upload API Key *** 
+//define("APIKEY","13E07AeeBF32411DBEB15E413eCDbAbA",TRUE);       //need change to your **Batch File Upload API Key *** 
+define("APIKEY","13E07AeeBF32411DBEB15E413eCDbAbb",TRUE);       //need change to your **Batch File Upload API Key *** 
 
 
 //Debug control
 define("MY_DEBUG",TRUE,TRUE); 
-
-//eft boundary string
-define ("BOUNDARY_STRING", "WebKitFormBoundaryeeBF32411");
-
-
-//eft_params in array 
-global $my_eft_records;
-$my_eft_records = array (
-        array(
-                'type'=> 'E',       //Transaction type - The type of transaction.E - EFT
-                'credit-debit'=>'D',//Transaction type ,C – Credit recipient bank accounts,D – Debit an outside bank account and depositing funds into your own
-                'bank'=>'000',         //Financial institution number - The 3 digit financial institution number
-                'transit'=>'00000',      //Bank transit number - The 5 digit bank transit number
-                'acc'=>'0000000000',          //Account number - The 5-12 digit account number
-                'amt'=>'0',          //Amount - Transaction amount in pennies
-                'ref'=>'',          //Reference number - An optional reference number of up to 19 digits. If you don't want a reference number, enter "0" (zero).
-                'name'=>'',         //Recipient name - Full name of the bank account holder
-                'code'=>'',          //Customer code - The 32-character customer code located in the Payment Profile. Do not populate bank account fields in the file when processing against a Payment Profile.
-                'desc'=>'',     
-            ),                
-        );
-
-//eft processing,1- now, otherwise $process_date is the date to process, Format YYYYMMDD,    
-$process_now = 1;
-$process_date = '';
 
 ?>
