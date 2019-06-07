a61994a848d0ac1a9d7f2dd69b2cabdcde0391a2
Leon Tang <leontang@opensrci.org>
2019-06-07 16:26:24 +0000

    clean up

--- sample.php
+++ sample.php
@@ -1,7 +1,7 @@
 <?php
 /*MIT License
 
-Copyright (c) 2019 leontang8911
+Copyright (c) 2019 OpenSRCi
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
